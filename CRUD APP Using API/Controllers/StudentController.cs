3f48d974c4445935e69a4028a897ff8b28e94529
ssakibs04 <sakibshadman34@gmail.com>
2024-09-01 21:59:26 +0600

    Initial

---
+++ CRUD APP Using API/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using CRUD_APP_Using_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CRUD_APP_Using_API.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7188/api/StudentAPI/";
+        private HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode) { 
+            
+            string result=response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Student>>(result);
+                if (data != null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+        }
+        [HttpGet]
+        public IActionResult Create() { 
+        return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Student std) { 
+            string data = JsonConvert.SerializeObject(std);
+            StringContent content = new StringContent(data,Encoding.UTF8,"application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode) {
+                TempData["insert_message"] = "Student Added";
+                return RedirectToAction("Index");
+            }
+        return View();
+        }
+       
+}
+    }
+
---
+++ CRUD APP Using API/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_APP_Using_API.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Gender { get; set; }
+        [Required]
+        public int age { get; set; }
+        [Required]
+        public int Standard { get; set; }
+
+
+
+
+    }
+}
